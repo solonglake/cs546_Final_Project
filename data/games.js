2a083ea00e92d68c5a9b5e1a1ab8772df69c869d
chaaf <chaaf@stevens.edu>
2022-04-28 17:23:29 -0400

    Please enter the commit message for your changes. Lines starting This branch is in progress but here is what I have for data in

--- data/games.js
+++ data/games.js
@@ -1,130 +1,70 @@
 const mongoCollections = require('../config/mongoCollections');
 const {ObjectId} = require('mongodb');
 const users = mongoCollections.users;
+const games = mongoCollections.games;
 const bcrypt = require('bcrypt');
 
 let exportedMethods = {
-    async createUser(username, password){
+    async createGame(gameName, gameImage){
         // input format checking
-        if(!username){
-            throw 'Username must be supplied!';
+        if(!gameName){
+            throw 'gameName must be supplied!';
         }
-        if(!password){
-            throw 'Password must be supplied!';
+        if(!gameImage){
+            throw 'Game Image must be supplied!';
         }
-        if(typeof(username) != 'string'){
-            throw 'Username must be a string!';
+        if(typeof(gameName) != 'string'){
+            throw 'gameName must be a string!';
         }
-        username = username.trim();
-        if(username.length < 4){
-            throw 'Username must atleast 4 characters long!';
+        gameName = gameName.trim();
+        if(gameName.length < 1){
+            throw 'gameName must atleast 2 characters long!';
         }
-        if(username.indexOf(' ') != -1){
-            throw 'Username cannot contain spaces!';
+        if(gameName.match(/^[0-9A-Za-z]+$/) === null){
+            throw 'gameName must only use alphanumeric characters!';
         }
-        if(username.match(/^[0-9A-Za-z]+$/) === null){
-            throw 'Username must only use alphanumeric characters!';
+        gameName = gameName.toLowerCase();
+        
+        let res = gameImage.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+        if(res === null){
+            throw 'Supplied link is not valid!';
         }
-        username = username.toLowerCase();
-        if(typeof(password) != 'string'){
-            throw 'Password must be a string!';
+        // check if game already exists in the database
+        const gamesCollection = await games();
+        const game = await gamesCollection.findOne({name: gameName});
+        if(game != null){
+            throw 'This game already exists';
         }
-        password = password.trim();
-        if(password.length < 6){
-            throw 'Password must atleast 6 characters long!';
-        }
-        if(password.indexOf(' ') != -1){
-            throw 'Password cannot contain spaces!';
-        }
-
-        // check if username already exists in the database
-        const usersCollection = await users();
-        const user = await usersCollection.findOne({username: username});
-        if(user != null){
-            throw 'Already user with the supplied username!';
-        }
-
-        // hash password and add user to database
-        const hashedPassword = await bcrypt.hash(password, 16);
-        let newUser = {
-            username: username,
-            password: hashedPassword
-        };
-        const insertInfo = await usersCollection.insertOne(newUser);
+        //we can check for the image too but that seems unUseful
+        // actually add the game
+        const insertInfo = await gameCollectiion.insertOne({'name':gameName, 'gameImage': gameImage, 'runs':[]});
         if(insertInfo.insertedCount === 0){
-            throw 'Could not add user!';
+            throw 'Could add game!';
         }
         return {userInserted: true};
     },
 
-    async checkUser(username, password){
+    async changeRunsArray(gameName, runs){
         // input format checking
-        if(!username){
-            throw 'Username must be supplied!';
+        if(!gameName){
+            throw 'gameName must be supplied!';
         }
-        if(!password){
-            throw 'Password must be supplied!';
+        if(typeof(gameName) != 'string'){
+            throw 'gameName must be a string!';
         }
-        if(typeof(username) != 'string'){
-            throw 'Username must be a string!';
-        }
-        username = username.trim();
-        if(username.length < 4){
+        gameName = gameName.trim();
+        if(gameName.length < 4){
             throw 'Username must atleast 4 characters long!';
         }
-        if(username.indexOf(' ') != -1){
+        if(gameName.indexOf(' ') != -1){
             throw 'Username cannot contain spaces!';
         }
-        if(username.match(/^[0-9A-Za-z]+$/) === null){
+        if(gameName.match(/^[0-9A-Za-z]+$/) === null){
             throw 'Username must only use alphanumeric characters!';
         }
-        username = username.toLowerCase();
-        if(typeof(password) != 'string'){
-            throw 'Password must be a string!';
-        }
-        password = password.trim();
-        if(password.length < 6){
-            throw 'Password must atleast 6 characters long!';
-        }
-        if(password.indexOf(' ') != -1){
-            throw 'Password cannot contain spaces!';
-        }
-
-        // check if username and password combination are in the database
-        const usersCollection = await users();
-        const user = await usersCollection.findOne({username: username});
-        if(user === null){
-            throw 'Either the username or password is invalid';
-        }
-        const matching = await bcrypt.compare(password, user.password);
-        if(!matching){
-            throw 'Either the username or password is invalid';
-        }
-        return {authenticated: true};
-    },
-
-    async changeBio(username, bio){
-        // input format checking
-        if(!username){
-            throw 'Username must be supplied!';
-        }
-        if(typeof(username) != 'string'){
-            throw 'Username must be a string!';
-        }
-        username = username.trim();
-        if(username.length < 4){
-            throw 'Username must atleast 4 characters long!';
-        }
-        if(username.indexOf(' ') != -1){
-            throw 'Username cannot contain spaces!';
-        }
-        if(username.match(/^[0-9A-Za-z]+$/) === null){
-            throw 'Username must only use alphanumeric characters!';
-        }
-        username = username.toLowerCase();
-        bio = bio.trim();
-        if(bio.length === 0){
-            throw 'Bio must not be empty!';
+        gameName = gameName.toLowerCase();
+        if(runs.length === 0){
+            throw 'runs must not be empty!';
         }
 
         // update user with new bio
