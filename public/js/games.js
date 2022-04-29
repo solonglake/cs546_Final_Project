2d75a1389cdb6de9e9638c3f50bd22a8dd4a8615
chaaf <chaaf@stevens.edu>
2022-04-29 17:06:18 -0400

    The jquery is linked to games page now. Also added form for adding game

--- public/js/games.js
+++ public/js/games.js
@@ -2,8 +2,11 @@
 (async function ($) {
     // the unordered list of games
     let games = $('#allGames');
+    let gamesDiv = $('#normalGames');
     // the add game button
     let adder = $('#gamePic');
+    let addDiv = $('#adding');
+
 
     let status = await $.ajax({
         url: '/authenticated',
@@ -14,107 +17,20 @@
     } else {
         adder.hide();
     }
+    adder.on("click", function () {
+        gamesDiv.hide();
+        addDiv.show();
+    });
 
     let all = await $ajax({
         url: '/games/getAll',
         type: 'Get'
     });
+    console.log("outside the loop")
     for (let x in all) {
+        console.log("is this printing")
         let temp = '<li>' + x.gameImage + '</li>'
         games.append(temp);
     }
 
-        // load users profile picture
-        const link = await $.ajax({
-            url: '/profile/profilePic',
-            type: 'Get'
-        });
-        if(link.link){
-            profilePic.attr("src", link.link);
-        }
-    
-        // load users bio
-        const userBio = await $.ajax({
-            url: '/profile/bio',
-            type: 'Get'
-        });
-        if(userBio.bio){
-            bio.text(userBio.bio);
-        }
-    
-        // load users posts
-        const posts = await $.ajax({
-            url: '/profile/posts',
-            type: 'Get'
-        });
-        if(posts.posts){
-            console.log('work in progress');
-        } else {
-            postsDiv.text('No posts yet');
-        }
-    
-        // load users runs
-        const runs = await $.ajax({
-            url: '/profile/runs',
-            type: 'Get'
-        });
-        if(runs.runs){
-            console.log('work in progress');
-        } else {
-            runsDiv.text('No runs yet');
-        }
-    
-        // profile pic click function
-        profilePic.click(function() {
-            profilePicForm.show();
-        });
-    
-        // bio click function
-        bio.click(function() {
-            bioForm.show();
-        });
-    
-        // help button submit form action
-        helpForm.submit(async function (event) {
-            event.preventDefault();
-            if(help.is(":visible")) {
-                help.hide();
-            } else {
-                help.show();
-            }
-        });
-    
-        // profile pic form submit action
-        profilePicForm.submit(async function (event) {
-            event.preventDefault();
-            let data = { profilePicInput: profilePicInput.val() };
-            profilePicForm[0].reset()
-            profilePicForm.hide(); 
-    
-            // profilePicInput validation
-            let validInput = true;
-            let res = data.profilePicInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
-            if(res === null){
-                validInput = false;
-            }
-    
-            // if profilePicInput is valid then make the ajax request to /profile/profilePic route
-            if(validInput){
-                profileError.hide();
-                const status = await $.ajax({
-                    url: '/profile/profilePic',
-                    type: 'Post',
-                    data: data
-                });
-                if(status.success){
-                    profilePic.attr("src", data.profilePicInput);
-                } else {
-                    profileError.text('Could not update profile picture with supplied link!');
-                    profileError.show();
-                }
-            } else {
-                profileError.text('Supplied link is not valid!');
-                profileError.show();
-            }
-        });
 })(window.jQuery);
