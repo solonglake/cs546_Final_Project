c302444bf722959c0a1b7d300f8989313b6b35c1
chaaf <chaaf@stevens.edu>
2022-04-29 13:16:42 -0400

    debug1

--- routes/games.js
+++ routes/games.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data');
+const usersData = data.users;
+const gamesData = data.games;
 
 router.get('/', async (req, res) => {
     try {
@@ -9,4 +12,29 @@ router.get('/', async (req, res) => {
     }
 });
 
+router.get('/getAll', async (req,res) => {
+    try {
+        let allGames = await gamesData.getAllGames();
+        res.json(allGames)
+    } catch (e) {
+        res.sendStatus(500);
+    }
+})
+
+// router.post('/gamePic', async (req, res) => {
+//     try {
+//         let gamePicInput = req.body.gamePicImnput;
+//         let gameName = req.body.gameName;
+//         let result = gamePicInput.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+//         let status;
+//         if(result === null){
+//             status = {success: false};
+//         } else {
+//             status = await gamesData.createGame(gameName, gamePicInput);    
+//         }
+//         res.json(status);
+//     } catch (e) {     
+//         res.sendStatus(500);
+//     }
+// });
 module.exports = router;
