14ad7f816bce72a3e7500c3216b44f47b5becbd1
Tristan Kensinger <tristan.kensinger@gmail.com>
2022-05-03 20:09:17 -0400

    Added the diagnostics extra feature onto the home page

--- public/js/home.js
+++ public/js/home.js
@@ -2,6 +2,10 @@
 (async function ($) {
     let loginForm = $('#loginForm');
     let profileForm = $('#profileForm');
+    let usersCount = $('#usersCount');
+    let runsCount = $('#runsCount');
+    let gamesCount = $('#gamesCount');
+    let forumsCount = $('#forumsCount');
 
     let status = await $.ajax({
         url: '/authenticated',
@@ -12,4 +16,29 @@
     } else {
         profileForm.hide();
     }
+
+    let totalUsers = await $.ajax({
+        url: '/totalUsers',
+        type: 'Post'
+    });
+    usersCount.text(totalUsers.totalUsers);
+
+    let totalRuns = await $.ajax({
+        url: '/totalRuns',
+        type: 'Post'
+    });
+    runsCount.text(totalRuns.totalRuns);
+
+    let totalGames = await $.ajax({
+        url: '/totalGames',
+        type: 'Post'
+    });
+    gamesCount.text(totalGames.totalGames);
+
+    let totalForums = await $.ajax({
+        url: '/totalForums',
+        type: 'Post'
+    });
+    forumsCount.text(totalForums.totalForums);
+
 })(window.jQuery);
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const data = require('../data');
+const homeData = data.home;
 
 router.get('/', async (req, res) => {
     try {
         res.render('partials/home', { title: 'Split', js: 'home.js' });
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
+
+router.post('/totalUsers', async (req, res) => {
+    try {
+        const totalUsers = await homeData.totalUsers();
+        res.json({totalUsers: totalUsers});
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
+
+router.post('/totalRuns', async (req, res) => {
+    try {
+        const totalRuns = await homeData.totalRuns();
+        res.json({totalRuns: totalRuns});
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
+
+router.post('/totalGames', async (req, res) => {
+    try {
+        const totalGames = await homeData.totalGames();
+        res.json({totalGames: totalGames});
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
+
+router.post('/totalForums', async (req, res) => {
+    try {
+        const totalForums = await homeData.totalForums();
+        res.json({totalForums: totalForums});
     } catch (e) {
         res.sendStatus(500);
     }
